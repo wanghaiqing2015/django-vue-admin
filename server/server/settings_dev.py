d677db78751b7064e5e2ee25ab65a34f658bff46
Wang Haiqing <krman@163.com>
2021-05-27 00:23:02 +0800

    111

--- server/server/settings_dev.py
+++ server/server/settings_dev.py
@@ -1,18 +1,19 @@
 from .settings import *
 DEBUG = True
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'demo',
-    #     'USER': 'root',
-    #     'PASSWORD': '123456',
-    #     'HOST': 'localhost',
-    #     'PORT': '3306',
-    # }
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-     }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'inspect',
+        'USER': 'root',
+        'PASSWORD': 'Star*2020',
+        'HOST': 'www.wanghaiqing2020.com',
+        'PORT': '3306',
+    },
+ 
+     # 'default': {
+         # 'ENGINE': 'django.db.backends.sqlite3',
+         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     # }
 }
 
 # celery配置
