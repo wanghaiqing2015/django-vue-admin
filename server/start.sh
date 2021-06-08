1d73a74ee49d5c82b9ac0b2f8a4f01e5b89e377a
Wang Haiqing <krman@163.com>
2021-06-08 21:08:10 +0800

    111

--- server/start.sh
+++ server/start.sh
@@ -2,9 +2,9 @@
 if [ v"$DJANGO_ENV" == 'vdev' ]; then
     python manage.py makemigrations system  
     python manage.py migrate
-    python manage.py runserver 80
+    python manage.py runserver 8000
     else
     python manage.py migrate
     python manage.py collectstatic --noinput
-    gunicorn server.wsgi:application -w 4 -k gthread -b 0.0.0.0:80
+    gunicorn server.wsgi:application -w 4 -k gthread -b 0.0.0.0:8000
 fi
